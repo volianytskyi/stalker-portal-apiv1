cc8062dbfe02f03e5a5bdea21c634627672385a8
Sergey Volyanytsky <sergey.volyanytsky@gmail.com>
2017-12-19 00:00:17 +0200

    added StalkerPortalException and Base

---
+++ lib/Base.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * User: volyanytsky
+ * Date: 18.12.2017
+ * Time: 23:34
+ */
+
+use Rest\Rest;
+
+abstract class Base
+{
+    protected $resource;
+
+    protected $api;
+    public function __construct(Rest $api)
+    {
+        $this->api = $api;
+    }
+
+
+    /**
+     * @param string $jsonString
+     * @return array|null
+     * @throws StalkerPortalException
+     */
+    final protected function decodeAnswer($jsonString)
+    {
+        $answer = json_decode($jsonString, true);
+
+        if($answer['status'] === 'OK')
+        {
+            return $answer['results'];
+        }
+
+        if($answer['status'] === 'ERROR')
+        {
+            throw new StalkerPortalException($answer['error']);
+        }
+
+        return null;
+    }
+
+    /**
+     * @return bool
+     * @throws StalkerPortalException
+     */
+    final public function checkConnection()
+    {
+        try
+        {
+            $data = $this->api->get("");
+            $res = $this->decodeAnswer($data);
+        }
+        catch(StalkerPortalException $e)
+        {
+            if($e->getMessage() === 'Empty resource')
+            {
+                return true;
+            }
+            throw new StalkerPortalException($e->getMessage());
+        }
+
+        return false;
+    }
+}
---
+++ lib/StalkerPortalException.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * User: volyanytsky
+ * Date: 18.12.2017
+ * Time: 23:38
+ */
+class StalkerPortalException extends \Exception
+{
+
+}
